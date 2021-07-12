bd0f5386947633a9e488bdf7f7dfddab63c35861
bogdanlotuk <bogdanlotuk@gmail.com>
2021-07-12 17:37:14 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+let mySwiper = new Swiper (".swiper-container.swiper-advantages", {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    allowTouchMove: true,
+    breakpoints: {       
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        576: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        600: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+
+        1300: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1800: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        }
+
+    },
+});
